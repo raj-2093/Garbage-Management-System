bee70b06b8ccc94e4014aaa911d7987ba9692fb6
raj-2093 <lunagariyarajy2093@gmail.com>
2024-06-29 14:53:16 +0530

    register, authentication setup

---
+++ api_server/src/constants/generals.js
@@ -0,0 +1,11 @@
+export const userRole = {
+  RESIDENTS: "resident",
+  ADMINISTRATORS: "administrator",
+  GARBAGE_COLLECTOR: "garbage_collector",
+};
+
+export const userRoleList = [
+  userRole.RESIDENTS,
+  userRole.ADMINISTRATORS,
+  userRole.GARBAGE_COLLECTOR,
+];
